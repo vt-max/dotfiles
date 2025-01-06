bd5eed79a5b82b2c0a5b52ee44a1c6080ab518af
Max van der Kolk <max.vanderkolk@vortech.nl>
2025-01-06 07:11:17 +0100

    feat: Update color scheme and fix tmux undercurl

--- config/nvim/lua/config/lazy/colors.lua
+++ config/nvim/lua/config/lazy/colors.lua
@@ -1,17 +1,35 @@
 return {
-    {
-        -- "savq/melange-nvim",
-        -- lazy = false,
-        -- priority = 1000,
+    -- {
+    --     -- "savq/melange-nvim",
+    --     -- lazy = false,
+    --     -- priority = 1000,
+    --
+    --     "folke/tokyonight.nvim",
+    --     lazy = false,
+    --     priority = 1000,
+    --     opts = {},
+    --
+    --     config = function()
+    --         vim.cmd("set termguicolors")
+    --         vim.cmd("colorscheme tokyonight-night")
+    --     end
+    -- }
 
-        "folke/tokyonight.nvim",
+    {
+        "ellisonleao/gruvbox.nvim",
         lazy = false,
         priority = 1000,
         opts = {},
 
+
         config = function()
+            require("gruvbox").setup({
+                contrast = "hard",
+                transparent_mode = true,
+            })
+
             vim.cmd("set termguicolors")
-            vim.cmd("colorscheme tokyonight-night")
+            vim.cmd("colorscheme gruvbox")
         end
     }
 }
