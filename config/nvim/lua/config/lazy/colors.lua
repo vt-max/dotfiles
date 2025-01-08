5e68abfab2c8d7f9a8e8b9c40a200677c6a38cbd
Max van der Kolk <max.vanderkolk@vortech.nl>
2025-01-08 09:07:31 +0100

    feat: Move to catppuccin colorscheme

--- config/nvim/lua/config/lazy/colors.lua
+++ config/nvim/lua/config/lazy/colors.lua
@@ -1,35 +1,52 @@
 return {
+    {
+        -- "savq/melange-nvim",
+        -- lazy = false,
+        -- priority = 1000,
+
+        -- "folke/tokyonight.nvim", lazy = false,
+        -- priority = 1000,
+        -- opts = {},
+        --
+        -- config = function()
+        --     vim.cmd("set termguicolors")
+        --     vim.cmd("colorscheme tokyonight-night")
+        -- end
+
+        "catppuccin/nvim",
+        priority = 1000,
+
+        config = function()
+            require("catppuccin").setup({
+                flavour = "mocha",
+                no_italic = true,
+            })
+            vim.cmd("colorscheme catppuccin")
+        end
+    }
+
     -- {
-    --     -- "savq/melange-nvim",
-    --     -- lazy = false,
-    --     -- priority = 1000,
-    --
-    --     "folke/tokyonight.nvim",
+    --     "ellisonleao/gruvbox.nvim",
     --     lazy = false,
     --     priority = 1000,
     --     opts = {},
     --
+    --
     --     config = function()
+    --         require("gruvbox").setup({
+    --             contrast = "hard",
+    --             italic = {
+    --                 strings = false,
+    --                 emphasis = false,
+    --                 comments = false,
+    --                 operators = false,
+    --                 folds = false,
+    --             },
+    --             transparent_mode = true,
+    --         })
+    --
     --         vim.cmd("set termguicolors")
-    --         vim.cmd("colorscheme tokyonight-night")
+    --         vim.cmd("colorscheme gruvbox")
     --     end
     -- }
-
-    {
-        "ellisonleao/gruvbox.nvim",
-        lazy = false,
-        priority = 1000,
-        opts = {},
-
-
-        config = function()
-            require("gruvbox").setup({
-                contrast = "hard",
-                transparent_mode = true,
-            })
-
-            vim.cmd("set termguicolors")
-            vim.cmd("colorscheme gruvbox")
-        end
-    }
 }
