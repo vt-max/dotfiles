534c99d8910a54c769bf69f01a969616def89f38
Max van der Kolk <max.vanderkolk@vortech.nl>
2025-02-27 07:55:36 +0100

    fix(diffview): Add missing dependencies for icons

--- config/nvim/lua/config/lazy/diffview.lua
+++ config/nvim/lua/config/lazy/diffview.lua
@@ -2,6 +2,10 @@ return {
     {
         "sindrets/diffview.nvim",
 
+        dependencies = {
+            { "nvim-tree/nvim-web-devicons" },
+        },
+
         config = function()
             require("diffview").setup({
                 use_icons = true,
